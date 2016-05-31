5f3e7e1ff509b269167928f0c80ec06a0343c0ad
Luke <adrian.veidt42@gmail.com>
2016-05-31 16:31:45 +0200

    empty weight method

--- Procedure.py
+++ Procedure.py
@@ -436,16 +436,15 @@ def post_process(nexus):
     summary.total_mission_time = (results.base.segments[-1].conditions.frames.inertial.time[-1] -
                                   results.base.segments[0].conditions.frames.inertial.time[0])
 
-    summary.MTOW_delta = zero_fuel_weight[0] + summary.base_mission_fuelburn - vehicle.mass_properties.takeoff
+    summary.MTOW_delta = zero_fuel_weight + summary.base_mission_fuelburn - vehicle.mass_properties.takeoff
 
     # summary.engine_weight =
 
     # print results.base.segments.keys()
     # print summary.engine_weight
 
-    print "zero fuel weight: ", zero_fuel_weight[0], "kg  i.e. (", payload, "+", operating_empty[0], ")"
-    print "MTOW selected: ", vehicle.mass_properties.takeoff, "kg, MTOW calculated: ", zero_fuel_weight[
-                                                                                           0] + summary.base_mission_fuelburn
+    print "zero fuel weight: ", zero_fuel_weight, "kg  i.e. (", payload, "+", operating_empty, ")"
+    print "MTOW selected: ", vehicle.mass_properties.takeoff, "kg, MTOW calculated: ", zero_fuel_weight + summary.base_mission_fuelburn
     # FIXME Fuel burn doesn't change when the operating empty weight changes!
     print "Max/Min throttle: ", summary.max_throttle, ", ", summary.min_throttle
     print "Take-off field length: ", summary.field_length_takeoff[0], "m"
@@ -547,7 +546,7 @@ def post_process(nexus):
     #     gt_engine.mass_properties.mass/1.6/gt_engine.number_of_engines,  #PER ENGINE DRY WEIGHT
     #
     #     summary.base_mission_fuelburn,
-    #     zero_fuel_weight[0] + summary.base_mission_fuelburn,
+    #     zero_fuel_weight + summary.base_mission_fuelburn,
     #     operating_empty,
     #     design_landing_weight,
     #     vehicle.weight_breakdown.wing,
