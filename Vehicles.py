1e1795e1f7436b5d5dddf146541c076c03ed551f
Stanimir <stanlalovstoev@gmail.com>
2016-06-16 14:51:49 +0200

    updated fuselage length

--- Vehicles.py
+++ Vehicles.py
@@ -92,7 +92,7 @@ def base_setup():
 
     fuselage.lengths.nose = 7.0
     fuselage.lengths.cabin = 10
-    fuselage.lengths.tail = 29.79 - (0.75 * 7.61) + 0.9 * 7.48
+    fuselage.lengths.tail = 29.79 - (0.75 * 7.61) + 0.90 * 7.48
     # print 'l_tail = ', fuselage.lengths.tail
     fuselage.lengths.total = fuselage.lengths.nose + fuselage.lengths.cabin + fuselage.lengths.tail
     fuselage.lengths.fore_space = 0.
